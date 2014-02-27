9d2d4bc9e6595248385924c4dae363a9c7c00569
Davor Ocelic <docelic-taskman@spinlocksolutions.com>
2014-02-27 15:30:16 +0100

    Resolve merge conflicts

--- theme/alpine/window/colortest/body.rb
+++ theme/alpine/window/colortest/body.rb
@@ -5,22 +5,11 @@ module TASKMAN
 		def initialize arg= {}
 			super
 
-			#v= Vbox.new( :@style_normal => 'bg=black,fg=white')
-			#v<< l= List.new
-			#l<< ListItem.new( '.height' => 3, :height => 3, :text => 'BADABADABOY!!!')
-			#l<< ListItem.new( '.height' => 3, :height => 3, :text => 'BADABADABOY!!!2')
-			#l<< ListItem.new( '.height' => 3, :height => 3, :text => 'BADABADABOY!!!3')
-			#l<< ListItem.new( '.height' => 3, :height => 3, :text => 'BADABADABOY!!!4')
-			#l<< ListItem.new( '.height' => 3, :height => 3, :text => 'BADABADABOY!!!5')
-			#l<< ListItem.new( '.height' => 3, :height => 3, :text => 'BADABADABOY!!!6')
-			#self<< v
-
 			@widget= nil
 
-			require arg[:body].to_s.lc
-			w= eval "#{arg[:body].ucfirst}.new"
-			#w= arg[:body].ucfirst.to_class.new
-			self<< w
+			require 'colors'
+			colors= Colors.new
+			self<< colors
 		end
 
 	end
